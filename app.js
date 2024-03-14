3659c8b73bc44f504b6cf9db0346995cbf1d4026
MaazWebDev <maazkhan66376@gmail.com>
2024-03-14 15:24:17 +0500

    When user put karachi shows a message

---
+++ app.js
@@ -0,0 +1,11 @@
+const place = prompt("Enter The City Of Lights ; ");
+const province =("karachi");
+
+
+if(place === "karachi"){
+    document.write('<h1 style="text-align:center; font-family:cursive;">Welcome to The City Of Lights Karachi !</h1>')
+    document.write('<img src="download.jpg" class="centered">')
+}
+else{
+    alert("Check Again Please !\nHint : karachi");
+}
